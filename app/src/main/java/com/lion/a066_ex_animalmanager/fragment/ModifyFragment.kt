fb53b1a6a48a16c85299a05aa95338920c06ae4e
magicTurtle2411 <magicturtle2411@gmail.com>
2024-11-13 22:01:32 +0900

    ModifyFragment 기본 코드

--- app/src/main/java/com/lion/a066_ex_animalmanager/fragment/ModifyFragment.kt
+++ app/src/main/java/com/lion/a066_ex_animalmanager/fragment/ModifyFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lion.a066_ex_animalmanager.MainActivity
 import com.lion.a066_ex_animalmanager.R
+import com.lion.a066_ex_animalmanager.databinding.FragmentModifyBinding
 
-
+// 동물 정보 수정 화면
 class ModifyFragment : Fragment() {
+
+    lateinit var fragmentModifyBinding: FragmentModifyBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fragmentModifyBinding = FragmentModifyBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modify, container, false)
+        return fragmentModifyBinding.root
     }
 
 }
