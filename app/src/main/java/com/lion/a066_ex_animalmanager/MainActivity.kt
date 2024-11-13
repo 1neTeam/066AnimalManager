6cecf2b5ff64c6a26398f8815a1641a09d7634d2
bpv834 <bpv8342Gmail.com>
2024-11-13 20:38:11 +0900

    main fragment Test2

--- app/src/main/java/com/lion/a066_ex_animalmanager/MainActivity.kt
+++ app/src/main/java/com/lion/a066_ex_animalmanager/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val str = "test1"
+
         val str2 = "test2"
 
         // 첫 화면을 설정한다.
