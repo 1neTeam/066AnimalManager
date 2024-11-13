136e4bf270aa60155b5da5bb6bcca495903b2e6c
magicTurtle2411 <magicturtle2411@gmail.com>
2024-11-14 00:47:26 +0900

    ModifyFragment 수정 처리 메서드 추가

--- app/src/main/java/com/lion/a066_ex_animalmanager/fragment/ModifyFragment.kt
+++ app/src/main/java/com/lion/a066_ex_animalmanager/fragment/ModifyFragment.kt
@@ -1,11 +1,16 @@
 package com.lion.a066_ex_animalmanager.fragment
 
+import AnimalGender
+import AnimalType
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.lion.a061ex_roomdatabase.repository.AnimalRepository
+import com.lion.a061ex_roomdatabase.viewmodel.AnimalViewModel
 import com.lion.a066_ex_animalmanager.MainActivity
 import com.lion.a066_ex_animalmanager.R
 import com.lion.a066_ex_animalmanager.databinding.FragmentModifyBinding
@@ -63,9 +68,58 @@ class ModifyFragment : Fragment() {
             // 동물 데이터를 가져온다.
             CoroutineScope(Dispatchers.Main).launch {
                 val work1 = async(Dispatchers.IO) {
-                    AnimalRepository
+                    AnimalRepository.selectAnimalIfoByStudentIdx(mainActivity, animalIdx)
+                }
+                val animalViewModel = work1.await()
+
+                when(animalViewModel.animalType) {
+                    AnimalType.Animal_TYPE_DOG -> {
+                        toggleGroupTypeModifyFragment.check(R.id.buttonTypeDogModifyFragment)
+                    }
+                    AnimalType.Animal_TYPE_CAT -> {
+                        toggleGroupTypeModifyFragment.check(R.id.buttonTypeCatModifyFragment)
+                    }
+                    AnimalType.Animal_TYPE_PARROT -> {
+                        toggleGroupTypeModifyFragment.check(R.id.buttonTypeParrotModifyFragment)
+                    }
+                }
+                textFieldNameModifyFragment.editText?.setText(animalViewModel.animalName)
+                textFieldAgeModifyFragment.editText?.setText(animalViewModel.animalAge)
+
+                when(animalViewModel.animalGender) {
+                    AnimalGender.ANIMAL_GENDER_MALE -> {
+                        radioGroupGenderModifyFragment.check(R.id.radioButtonMaleModifyFragment)
+                    }
+                    AnimalGender.ANIMAL_GENDER_FEMALE -> {
+                        radioGroupGenderModifyFragment.check(R.id.radioButtonFemaleModifyFragment)
+                    }
                 }
             }
+        }
+    }
+
+    // 수정 처리 메서드
+    fun modifyDone() {
+        val materialAlertDialogBuilder = MaterialAlertDialogBuilder(mainActivity)
+        materialAlertDialogBuilder.setTitle("수정")
+        materialAlertDialogBuilder.setMessage("이전 데이터로 복원할 수 없습니다.")
+        materialAlertDialogBuilder.setNeutralButton("취소", null)
+        materialAlertDialogBuilder.setPositiveButton("수정") {dialogInterface:DialogInterface, i:Int ->
+            // 수정할 데이터
+            val animalIdx = arguments?.getInt("animalIdx")!!
+            val animalType = when(fragmentModifyBinding.toggleGroupTypeModifyFragment.checkedButtonId){
+                R.id.buttonTypeDogModifyFragment -> AnimalType.Animal_TYPE_DOG
+                R.id.buttonTypeCatModifyFragment -> AnimalType.Animal_TYPE_CAT
+                else -> AnimalType.Animal_TYPE_PARROT
+            }
+            val animalName = fragmentModifyBinding.textFieldNameModifyFragment.editText?.text.toString()
+            val animalAge = fragmentModifyBinding.textFieldAgeModifyFragment.editText?.text.toString().toInt()
+            val animalGender = when(fragmentModifyBinding.radioGroupGenderModifyFragment.checkedRadioButtonId) {
+                R.id.radioButtonMaleModifyFragment -> AnimalGender.ANIMAL_GENDER_MALE
+                else -> AnimalGender.ANIMAL_GENDER_FEMALE
+            }
+
+            val animalViewModel = AnimalViewModel(animalIdx, animalType, animalName, animalAge, animalGender, animalFavoriteSnack = String)
         }
     }
 
