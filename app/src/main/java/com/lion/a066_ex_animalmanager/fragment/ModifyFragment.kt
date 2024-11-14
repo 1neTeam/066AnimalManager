d1b91bc3abf7f263545e7c5e3049f339bb3de968
magicTurtle2411 <magicturtle2411@gmail.com>
2024-11-15 00:26:43 +0900

    ModifyFragment 수정

--- app/src/main/java/com/lion/a066_ex_animalmanager/fragment/ModifyFragment.kt
+++ app/src/main/java/com/lion/a066_ex_animalmanager/fragment/ModifyFragment.kt
@@ -1,11 +1,18 @@
 package com.lion.a066_ex_animalmanager.fragment
 
+import AnimalFood
+import AnimalGender
+import AnimalType
+import FragmentName
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.lion.a061ex_roomdatabase.repository.AnimalRepository
+import com.lion.a061ex_roomdatabase.viewmodel.AnimalViewModel
 import com.lion.a066_ex_animalmanager.MainActivity
 import com.lion.a066_ex_animalmanager.R
 import com.lion.a066_ex_animalmanager.databinding.FragmentModifyBinding
@@ -29,6 +36,8 @@ class ModifyFragment : Fragment() {
 
         // 툴바 설정 메서드 호출
         settingToolbar()
+        // 입력 요소들 초기 설정
+        settingInput()
 
         return fragmentModifyBinding.root
     }
@@ -47,7 +56,7 @@ class ModifyFragment : Fragment() {
             toolbarModify.setOnMenuItemClickListener {
                 when(it.itemId) {
                     R.id.modify_toolbar_menu_done -> {
-                        mainActivity.removeFragment(FragmentName.MODIFY_FRAGMENT)
+                        modifyDone()
                     }
                 }
                 true
@@ -63,10 +72,89 @@ class ModifyFragment : Fragment() {
             // 동물 데이터를 가져온다.
             CoroutineScope(Dispatchers.Main).launch {
                 val work1 = async(Dispatchers.IO) {
-                    AnimalRepository
+                    AnimalRepository.selectAnimalIfoByStudentIdx(mainActivity, animalIdx)
+                }
+                val animalViewModel = work1.await()
+
+                when(animalViewModel.animalType) {
+                    AnimalType.Animal_TYPE_DOG -> {
+                        toggleGroupTypeModifyFragment.check(R.id.buttonTypeDogModifyFragment)
+                    }
+                    AnimalType.Animal_TYPE_CAT -> {
+                        toggleGroupTypeModifyFragment.check(R.id.buttonTypeCatModifyFragment)
+                    }
+                    AnimalType.Animal_TYPE_PARROT -> {
+                        toggleGroupTypeModifyFragment.check(R.id.buttonTypeParrotModifyFragment)
+                    }
+                }
+                textFieldNameModifyFragment.editText?.setText(animalViewModel.animalName)
+                textFieldAgeModifyFragment.editText?.setText(animalViewModel.animalAge)
+
+                when(animalViewModel.animalGender) {
+                    AnimalGender.ANIMAL_GENDER_MALE -> {
+                        radioGroupGenderModifyFragment.check(R.id.radioButtonMaleModifyFragment)
+                    }
+                    AnimalGender.ANIMAL_GENDER_FEMALE -> {
+                        radioGroupGenderModifyFragment.check(R.id.radioButtonFemaleModifyFragment)
+                    }
+                }
+
+                animalViewModel.animalFavoriteSnack.forEach { snack ->
+                    when (snack) {
+                        AnimalFood.FOOD_APPLE -> {
+                            chipGroupSnacksModifyFragment.check(R.id.chipAppleModifyFragment)
+                        }
+                        AnimalFood.FOOD_BANANA -> {
+                            chipGroupSnacksModifyFragment.check(R.id.chipBananaModifyFragment)
+                        }
+                        AnimalFood.FOOD_ORANGE -> {
+                            chipGroupSnacksModifyFragment.check(R.id.chipOrangeModifyFragment)
+                        }
+                    }
                 }
             }
         }
     }
 
+    // 수정 처리 메서드
+    fun modifyDone() {
+        val materialAlertDialogBuilder = MaterialAlertDialogBuilder(mainActivity)
+        materialAlertDialogBuilder.setTitle("수정")
+        materialAlertDialogBuilder.setMessage("이전 데이터로 복원할 수 없습니다.")
+        materialAlertDialogBuilder.setNeutralButton("취소", null)
+        materialAlertDialogBuilder.setPositiveButton("수정") {dialogInterface:DialogInterface, i:Int ->
+            // 수정할 데이터
+            val animalIdx = arguments?.getInt("animalIdx")!!
+            val animalType = when(fragmentModifyBinding.toggleGroupTypeModifyFragment.checkedButtonId){
+                R.id.buttonTypeDogModifyFragment -> AnimalType.Animal_TYPE_DOG
+                R.id.buttonTypeCatModifyFragment -> AnimalType.Animal_TYPE_CAT
+                else -> AnimalType.Animal_TYPE_PARROT
+            }
+            val animalName = fragmentModifyBinding.textFieldNameModifyFragment.editText?.text.toString()
+            val animalAge = fragmentModifyBinding.textFieldAgeModifyFragment.editText?.text.toString().toInt()
+            val animalGender = when(fragmentModifyBinding.radioGroupGenderModifyFragment.checkedRadioButtonId) {
+                R.id.radioButtonMaleModifyFragment -> AnimalGender.ANIMAL_GENDER_MALE
+                else -> AnimalGender.ANIMAL_GENDER_FEMALE
+            }
+            val animalFavoriteSnack = mutableListOf<AnimalFood>()
+            fragmentModifyBinding.chipGroupSnacksModifyFragment.checkedChipIds.forEach {
+                when (it) {
+                    R.id.chipAppleModifyFragment -> animalFavoriteSnack.add(AnimalFood.FOOD_APPLE)
+                    R.id.chipBananaModifyFragment -> animalFavoriteSnack.add(AnimalFood.FOOD_BANANA)
+                    R.id.chipOrangeModifyFragment -> animalFavoriteSnack.add(AnimalFood.FOOD_ORANGE)
+                }
+            }
+
+            val animalViewModel = AnimalViewModel(animalIdx, animalType, animalName, animalAge, animalGender, animalFavoriteSnack)
+
+            CoroutineScope(Dispatchers.Main).launch {
+                val work1 = async(Dispatchers.IO) {
+                    AnimalRepository.updateAnimalInfo(mainActivity, animalViewModel)
+                }
+                work1.join()
+                mainActivity.removeFragment(FragmentName.MODIFY_FRAGMENT)
+            }
+        }
+        materialAlertDialogBuilder.show()
+    }
 }
